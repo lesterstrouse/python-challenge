7a02c39d05600b8adedac3dd0c914b3bd60d65ba
lesterstrouse <47232890+lesterstrouse@users.noreply.github.com>
2019-03-06 21:53:45 -0800

    commit 3/6

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,3 +1,51 @@
-#this is the main script 
-import csv
+
+# coding: utf-8
+
+# In[11]:
+
+
 import os
+import csv
+
+
+# In[19]:
+
+
+count = 0
+total = 0
+gincrease = 0
+gdecrease = 0
+
+
+# In[12]:
+
+
+csv_path = os.path.join("..", "budget_data.csv")
+
+
+# In[26]:
+
+
+with open(csv_path) as csv_file:
+    csv_reader = csv.reader(csv_file)
+    csv_header = next(csv_reader)
+    #print(csv_header)
+    for row in csv_reader:
+        count+=1
+        amt=int(row[1])
+        total+=amt
+        if amt > gincrease:
+            gincrease = amt
+        elif amt < gdecrease:
+            gdecrease = amt
+
+
+# In[28]:
+
+
+print(f"no of months {count}")
+print(f"total increase/decrease {total}")
+print(f"average increase/decrease {total/count}")
+print(f"greatest increase {gincrease}")
+print(f"greatest decrease {gdecrease}")
+
--- PyPull/main.py
+++ PyPull/main.py
@@ -1,4 +1,42 @@
-#this is the main script
+
+# coding: utf-8
+
+# In[1]:
+
+
 import os
 import csv
+
+
+# In[2]:
+
+
+csv_path = os.path.join("..", "election_data.csv")
+
+
+# In[3]:
+
+
+count = 0
+total = 0
+votes = dict()
+
+
+# In[4]:
+
+
+with open(csv_path) as csv_file:
+    csv_reader = csv.reader(csv_file)
+    csv_header = next(csv_reader)
+    #print(csv_header)
+    for row in csv_reader:
+        count+=1
+        votes[row[2]] = votes.get(row[2],0) + 1
+
+
+# In[6]:
+
+
+print(votes)
+print(count)
 
