2ba76a22bde4ab4636546f20c26f6dde3add8f42
lesterstrouse <47232890+lesterstrouse@users.noreply.github.com>
2019-03-07 15:14:12 -0800

    commit 3/8

--- PyPull/main.py
+++ PyPull/main.py
@@ -1,27 +1,27 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[2]:
 
 
 import os
 import csv
 
 
-# In[2]:
+# In[3]:
 
 
 csv_path = os.path.join("..", "election_data.csv")
 
 
-# In[3]:
+# In[4]:
 
 
 count = 0
 votes = dict()
 
 
-# In[4]:
+# In[5]:
 
 
 with open(csv_path) as csv_file:
@@ -33,16 +33,29 @@ with open(csv_path) as csv_file:
         votes[row[2]] = votes.get(row[2],0) + 1
 
 
-# In[42]:
+# In[7]:
 
 
 winvotes = 0
 for key in votes:
     pct = votes[key]/count*100
-    print(f"{key},{votes[key]},{pct:.1f}")
+    print(f"Candidate {key} with {votes[key]} votes had {pct:.1f}% of vote")
     if votes[key] > winvotes:
         winvotes = votes[key]
         winner = key
 print(f"total votes {count}")
 print(f"winner {winner}")
+
+
+# In[13]:
+
+
+text_path = os.path.join("..", "election_summ.txt")
+text_file = open(text_path,'w')
+for key in votes:
+    pct = votes[key]/count*100
+    text_file.write(f"Candidate {key} with {votes[key]} votes had {votes[key]/count*100:.1f}% of vote\n")
+text_file.write(f"total votes {count}\n")
+text_file.write(f"winner {winner}\n")
+text_file.close()
 
