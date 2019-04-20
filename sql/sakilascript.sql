274710febee4b334b1d11f4b7332432fc8b3b5a1
lesterstrouse <47232890+lesterstrouse@users.noreply.github.com>
2019-04-19 23:02:12 -0700

    commit 4/19

--- sql/sakilascript.sql
+++ sql/sakilascript.sql
@@ -27,3 +27,15 @@ select first_name, last_name, sum(amount) from customer c join payment p on c.cu
 select title from film where film_id in (select film_id from film where title like'q%' or title like 'k%')
 and language_id in (select language_id from language where name = 'english'); 
 select first_name, last_name from actor a where actor_id in (select actor_id from film_actor where film_id in (select film_id from film where title = 'alone trip'));
+select first_name, last_name, email from customer c join address a on c.address_id = a.address_id join city on a.city_id = city.city_id join country coun on city.country_id = coun.country_id where coun.country = 'canada';
+select title from film f join film_category fc on f.film_id = fc.film_id join category cat on fc.category_id = cat.category_id where name = 'family';
+select title, sum(p.amount) 'total amt' from film f join inventory i on f.film_id = i.film_id
+join rental r on i.inventory_id = r.inventory_id join payment p on r.rental_id = p.rental_id 
+group by title order by sum(p.amount) desc; 
+select * from sales_by_store;
+select store_id,city,country from store s join address a on s.address_id = a.address_id
+join city on a.city_id = city.city_Id join country ctry where city.country_id = ctry.country_id;
+select category, total_sales from sales_by_film_category order by total_sales desc limit 5;
+create view Top5Genre as select category, total_sales from sales_by_film_category order by total_sales desc limit 5;
+select * from Top5Genre;
+drop view Top5Genre;
