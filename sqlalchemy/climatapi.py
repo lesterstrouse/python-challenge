671210735c43e4886dc37715b6c1804bc3c3ba62
lesterstrouse <47232890+lesterstrouse@users.noreply.github.com>
2019-04-29 16:14:39 -0700

    commit 4/29

--- sqlalchemy/climatapi.py
+++ sqlalchemy/climatapi.py
@@ -1,12 +1,20 @@
-from flask import flask
+from flask import flask, jsonify
 app = flask(__name__)
 # 3. Define what to do when a user hits the index route
 @app.route("/")
 def home():
     print("Server received request for 'Home' page...")
-    return "Welcome to my 'Home' page!"
-@app.route("/api/v1.0")
-@app.route("/api/v1.0")
-@app.route("/api/v1.0")
-@app.route("/api/v1.0")
-@app.route("/api/v1.0")
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation"
+        f"/api/v1.0/stations"
+        f"/api/v1.0/tobs"
+        f"/api/v1.0/<start>"
+        f"/api/v1.0/<start>/<end>"
+@app.route("/api/v1.0/precipitation")
+@app.route("/api/v1.0/stations")
+@app.route("/api/v1.0/tobs")
+@app.route("/api/v1.0/<start>")
+@app.route("/api/v1.0/<start>/<end>")
+if __name__ == "__main__":
+    app.run(debug=True)
